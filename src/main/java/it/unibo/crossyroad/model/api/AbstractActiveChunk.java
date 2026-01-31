1b15263b5f1aeec1e757dc80e840368a5cd7f366
Alessio Magnani <alessio.magnani@studio.unibo.it>
2026-01-31 22:24:00 +0100

    add method to determine new obstacle generation

--- src/main/java/it/unibo/crossyroad/model/api/AbstractActiveChunk.java
+++ src/main/java/it/unibo/crossyroad/model/api/AbstractActiveChunk.java
@@ -1,7 +1,7 @@
 package it.unibo.crossyroad.model.api;
 
 public abstract class AbstractActiveChunk extends AbstractChunk implements ActiveChunk {
-    
+
     public AbstractActiveChunk(Position initialPosition, Dimension dimension) {
         super(initialPosition, dimension);
     }
@@ -15,6 +15,18 @@ public abstract class AbstractActiveChunk extends AbstractChunk implements Activ
             if (obs instanceof ActiveObstacle) {
                 ((ActiveObstacle)obs).update(deltaTime, params);
             }
+
+            if (this.shouldGenerateNewObstacles(deltaTime)) {
+                this.generateObstacles();
+            }
         }
     }
+
+    /**
+     * A method to determine if new obstacles should be generated.
+     *
+     * @param deltaTime the time elapsed since the last update
+     * @return true if new obstacles should be generated, false otherwise
+     */
+    protected abstract boolean shouldGenerateNewObstacles(long deltaTime);
 }
