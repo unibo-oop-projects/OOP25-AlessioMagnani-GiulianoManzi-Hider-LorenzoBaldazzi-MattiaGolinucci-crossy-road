3ccef547507b160f5a7622a1367aa180ff8630d5
Mattia Golinucci <mattia.golinucci2@studio.unibo.it>
2026-02-02 11:01:06 +0100

    create GameView interface

---
+++ src/main/java/it/unibo/crossyroad/view/api/GameView.java
@@ -0,0 +1,29 @@
+package it.unibo.crossyroad.view.api;
+
+import it.unibo.crossyroad.model.api.EntityType;
+import it.unibo.crossyroad.model.api.Positionable;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Interface representing the game view.
+ */
+public interface GameView {
+    //void setController(GameController c);
+    //void render(List<Positionable> positionables);
+
+    /**
+     * Updates the power-up time display.
+     *
+     * @param powerUps a map of entity types to their remaining time
+     */
+    void updatePowerUpTime(Map<EntityType, Long> powerUps);
+
+    /**
+     * Updates the coin count display.
+     *
+     * @param count the current coin count
+     */
+    void updateCoinCount(int count);
+}
---
+++ src/main/java/it/unibo/crossyroad/view/impl/GameViewImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.crossyroad.view.impl;
+
+import it.unibo.crossyroad.model.api.EntityType;
+import it.unibo.crossyroad.model.api.Positionable;
+import it.unibo.crossyroad.view.api.GameView;
+
+import java.util.List;
+import java.util.Map;
+
+public class GameViewImpl implements GameView {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updatePowerUpTime(Map<EntityType, Long> powerUps) {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateCoinCount(int count) {
+
+    }
+}
